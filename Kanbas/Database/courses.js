454d5c17e05d83d1328268c48878ff3f2dcfea89
jjviasus <61569318+jjviasus@users.noreply.github.com>
2024-04-02 18:51:04 -0400

    assignment finished

---
+++ Kanbas/Database/courses.js
@@ -0,0 +1,58 @@
+export default [
+  {
+    "_id": "CS1234",
+    "name": "React JS",
+    "number": "CS1234",
+    "startDate": "2023-01-10",
+    "endDate": "2023-05-15",
+    "image": "reactjs.jpeg"
+  },
+  {
+    "_id": "MS1001",
+    "name": "Music Recording 1",
+    "number": "MS1001",
+    "startDate": "2023-01-10",
+    "endDate": "2023-05-15",
+    "image": "recording-music-1.jpeg"
+  },
+  {
+    "_id": "MS1002",
+    "name": "Music Recording 2",
+    "number": "MS1002",
+    "startDate": "2023-01-10",
+    "endDate": "2023-05-15",
+    "image": "music-recording-2.jpeg"
+  },
+  {
+    "_id": "MS1003",
+    "name": "Music Recording 3",
+    "number": "MS1003",
+    "startDate": "2023-01-10",
+    "endDate": "2023-05-15",
+    "image": "music-recording-3.jpeg"
+  },
+  {
+    "_id": "CS2000",
+    "name": "C++",
+    "number": "CS2000",
+    "startDate": "2023-01-10",
+    "endDate": "2023-05-15",
+    "image": "c++.jpeg"
+  },
+  {
+    "_id": "CS3000",
+    "name": "Android Development",
+    "number": "CS3000",
+    "startDate": "2023-01-10",
+    "endDate": "2023-05-15",
+    "image": "android-development.png"
+  },
+  {
+    "_id": "MS2000",
+    "name": "Logic Pro X",
+    "number": "MS2000",
+    "startDate": "2023-01-10",
+    "endDate": "2023-05-15",
+    "image": "logic-pro-x.jpeg"
+  }
+]
---
+++ Kanbas/Database/lectures.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    "id": 1,
+    "title": "Lecture",
+    "courseCode": "CS4550.12631.202410",
+    "date": "Sep 7 at 11:45am"
+  },
+  {
+    "id": 2,
+    "title": "Lecture",
+    "courseCode": "CS4550.12631.202410",
+    "date": "Sep 11 at 11:45am"
+  },
+  {
+    "id": 3,
+    "title": "Lecture",
+    "courseCode": "CS4550.12631.202410",
+    "date": "Sep 11 at 6pm"
+  }
+]
